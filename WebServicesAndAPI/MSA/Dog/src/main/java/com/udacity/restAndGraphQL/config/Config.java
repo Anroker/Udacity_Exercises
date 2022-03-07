59a03ddf9938e9a075f20c4a81c1e68ba68bf414
Anrok <glabmarcin94@gmail.com>
2022-03-07 13:21:35 +0100

    Documentation
    - Added swagger

--- WebServicesAndAPI/MSA/Dog/src/main/java/com/udacity/restAndGraphQL/config/Config.java
+++ WebServicesAndAPI/MSA/Dog/src/main/java/com/udacity/restAndGraphQL/config/Config.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,6 +28,17 @@ public class Config extends WebSecurityConfigurerAdapter {
                 .httpBasic();
         //for H2Database /console access
         http.headers().frameOptions().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+//        super.configure(web);
+        web.ignoring().antMatchers("/v3/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
     }
 
     @Override
